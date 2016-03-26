73c541cf7cec1f845e8f9f44a7baaec1b692d8c5
Kurniawan <Kurniawan@Kurniawan>
2016-03-27 02:24:46 +0700

    updated

--- BiroPerjalanan/src/biroperjalanan/PaketWisata.java
+++ BiroPerjalanan/src/biroperjalanan/PaketWisata.java
@@ -5,6 +5,7 @@
  */
 package biroperjalanan;
 
+import java.util.Arrays;
 /**
  *
  * @author Kurniawan
@@ -13,42 +14,58 @@ public class PaketWisata {
     private String idPaket;
     private TempatWisata[] daftarTujuanWisata;
     private int jumlahWisata;
+    private int harga;
 
     public PaketWisata(String idPaket) {
         this.idPaket = idPaket;
-        daftarTujuanWisata=new TempatWisata[3];
+        daftarTujuanWisata = new TempatWisata[3];
     }
-    
+
     public int getJumlahWisata() {
         return jumlahWisata;
     }
-    
+
     public String getIdPaket() {
         return idPaket;
     }
-    
-    public void addTempatWisata(TempatWisata w){
-        this.daftarTujuanWisata[jumlahWisata]=w;
+
+    public void addTempatWisata(TempatWisata w) {
+        this.daftarTujuanWisata[jumlahWisata] = w;
         jumlahWisata++;
     }
 
     public TempatWisata getTempatWisata(int i) {
         return daftarTujuanWisata[i];
     }
-    
-    public TempatWisata getTempatWisata(String id){
-        int i=0;
-        while (daftarTujuanWisata[i].getKode()!= id) {
+
+    public TempatWisata getTempatWisata(String id) {
+        int i = 0;
+        while (daftarTujuanWisata[i].getKode() != id) {
             i++;
         }
         return daftarTujuanWisata[i];
     }
-    public void removeTempatWisata(TempatWisata wst){
+
+    public void removeTempatWisata(TempatWisata wst) {
         for (int i = 0; i < daftarTujuanWisata.length; i++) {
-            if (daftarTujuanWisata[i]==wst) {
-                daftarTujuanWisata[i]=null;
+            if (daftarTujuanWisata[i] == wst) {
+                daftarTujuanWisata[i] = null;
             }
-            
+
         }
     }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    @Override
+    public String toString() {
+        return "PaketWisata{" + "idPaket=" + idPaket + ", daftarTujuanWisata=" + Arrays.toString(daftarTujuanWisata) + ", harga=" + harga + '}';
+    }
+
 }
