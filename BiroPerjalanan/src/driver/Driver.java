85b536d2440b072468b05b7af2e3f3d5e824d07a
Kurniawan <Kurniawan@Kurniawan>
2016-04-17 08:49:05 +0700

    updated

---
+++ BiroPerjalanan/src/driver/Driver.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+import biroperjalanan.Aplikasi;
+
+/**
+ *
+ * @author Kurniawan
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+        Aplikasi app = new Aplikasi();
+        app.mainMenu();
+
+    }
+}
---
+++ BiroPerjalanan/src/driver/Start.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+import control.Controller;
+
+/**
+ *
+ * @author Kurniawan
+ * 
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        Controller ctrl= new Controller();
+    }
+}
